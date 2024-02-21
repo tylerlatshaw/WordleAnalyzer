6ee5a8dcf54a670bff157cafd9b089425f5d109b
Tyler Latshaw <tylerlatshaw@gmail.com>
2024-02-21 18:43:52 -0500

    Commented out notion connection

--- src/app/api/get-all-words/route.tsx
+++ src/app/api/get-all-words/route.tsx
@@ -1,10 +1,10 @@
-import { Client } from "@notionhq/client";
+// import { Client } from "@notionhq/client";
 import { NextResponse } from "next/server";
 
 const notionSecret = process.env.NOTION_SECRET;
 const notionDatabaseId = process.env.NOTION_DB_LETTERS;
 
-const notion = new Client({ auth: notionSecret });
+// const notion = new Client({ auth: notionSecret });
 
 type Row = {
   Score: number,
@@ -16,19 +16,19 @@ export async function GET() {
 
   if (!notionSecret || !notionDatabaseId) throw new Error("Missing Notion Authentication");
 
-  const query = await notion.databases.query({
-    database_id: notionDatabaseId,
-    page_size: 2
-  });
+  // const query = await notion.databases.query({
+  //   database_id: notionDatabaseId,
+  //   page_size: 2
+  // });
 
   // @ts-ignore
   const data = query.results.map((row) => row.properties);
 
-  const structuredData: Row[] = data.map((row) => ({
-    Score: row.Score.number,
-    Position: row.Position.number,
-    Letter: row.Letter.title[0].plain_text
-  }));
+  // const structuredData: Row[] = data.map((row) => ({
+  //   Score: row.Score.number,
+  //   Position: row.Position.number,
+  //   Letter: row.Letter.title[0].plain_text
+  // }));
 
   return NextResponse.json(data);
 
